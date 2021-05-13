732957a730ebed4eac019a074e47de9a9a54ceb8
DabuAlexandru <adabu34@gmail.com>
2021-05-13 20:48:56 +0300

    Added functions to fetch the custom exercises from the database.

--- app/src/main/java/com/example/fitwork/AddExerciseActivity.java
+++ app/src/main/java/com/example/fitwork/AddExerciseActivity.java
@@ -16,14 +16,20 @@ import androidx.annotation.Nullable;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+
+import Models.Exercise;
 
 import static android.content.ContentValues.TAG;
 
@@ -32,41 +38,132 @@ public class AddExerciseActivity extends Activity {
     private Button GoBackToWorkoutButton;
     private ListView ChooseExercise;
 
+    private final ContextManager context = ContextManager.getInstance();
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private final FirebaseUser user = mAuth.getCurrentUser();
+
+    private ArrayList<Exercise> getDefaultExercises() {
+        ArrayList<Exercise> exercises = new ArrayList<>();
+        db.collection("exercises")
+            .get()
+            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "Successfully fetched exercises from database");
+                        QuerySnapshot queryDocumentSnapshots = task.getResult();
+                        for(DocumentSnapshot exercise : queryDocumentSnapshots) {
+                            String name             = exercise.getString("name");
+                            String description      = exercise.getString("description");
+                            List<String> muscles    = (List<String>) exercise.get("muscleGroups");
+                            exercises.add(new Exercise(name, description, muscles));
+                        }
+                        getCustomExercises(exercises); // propagate the list further to the next step
+                    } else {
+                        Log.d(TAG, "Failed to fetch exercises from database");
+                    }
+                }
+            });
+        return exercises;
+    }
+
+    private ArrayList<Exercise> getCustomExercises(ArrayList<Exercise> exercises) {
+        assert user != null;
+        String uid = user.getUid();
+        db.collection("profiles")
+                .whereEqualTo("uid", uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            Objects.requireNonNull(task.getResult())
+                                    .getDocuments()
+                                    .get(0)
+                                    .getReference()
+                                    .collection("privateProfile")
+                                    .get()
+                                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            if(task.isSuccessful()){
+                                                Objects.requireNonNull(task.getResult())
+                                                        .getDocuments()
+                                                        .get(0)
+                                                        .getReference()
+                                                        .collection("exercises")
+                                                        .get()
+                                                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    Log.d(TAG, "Successfully fetched exercises from database");
+                                                                    QuerySnapshot queryDocumentSnapshots = task.getResult();
+                                                                    for(DocumentSnapshot exercise : queryDocumentSnapshots) {
+                                                                        String name             = exercise.getString("name");
+                                                                        String description      = exercise.getString("description");
+                                                                        List<String> muscles    = (List<String>) exercise.get("muscleGroups");
+                                                                        exercises.add(new Exercise(name, description, muscles));
+                                                                    }
+                                                                    showExercises(exercises); // propagate the answer forward to the display function
+                                                                } else {
+                                                                    Log.d(TAG, "Failed to fetch exercises from database");
+                                                                }
+                                                            }
+                                                        });
+                                            }
+                                            else{
+                                                Log.d(TAG, "No private profile for user with uid " + uid);
+                                            }
+                                        }
+                                    });
+                        }
+                        else{
+                            Log.d(TAG, "No user with uid " + uid);
+                        }
+                    }
+                });
+        return exercises;
+    }
+
+    private void showExercises(ArrayList<Exercise> exercises) {
+        ArrayList<String> formattedExercises = formatExercises(exercises);
+        ArrayAdapter<String> adaptExercises = new ArrayAdapter<String>(
+                context,
+                android.R.layout.simple_list_item_1,
+                formattedExercises);
+        ChooseExercise.setAdapter(adaptExercises);
+    }
+
+    private ArrayList<String> formatExercises(ArrayList<Exercise> exercises) {
+        ArrayList<String> formattedExercises = new ArrayList<>();
+        for(Exercise exercise : exercises) {
+            String name = exercise.getName();
+            List<String> muscles = exercise.getMuscleGroups();
+            String result = name;
+            if (muscles != null) {
+                result += "\n" + muscles.stream()
+                        .map(i -> "" + i)
+                        .collect(Collectors.joining(", "));
+            }
+            formattedExercises.add(result);
+        }
+        for(String e : formattedExercises) {
+            Log.d(TAG, e + "\n");
+        }
+        return formattedExercises;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_exercise);
 
         ChooseExercise = (ListView) findViewById(R.id.chooseExercise);
-        ContextManager context = ContextManager.getInstance();
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("exercises")
-            .get()
-            .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                @Override
-                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                    ArrayList<String> exercises = new ArrayList<>();
-                    for(DocumentSnapshot exercise : queryDocumentSnapshots) {
-                        if(exercise.exists()) {
-                            String name = exercise.getString("name");
-                            List<String> muscles = (List<String>) exercise.get("muscleGroups");
-                            String result = name;
-                            if (muscles != null) {
-                                result += "\n" + muscles.stream()
-                                        .map(i -> "" + i)
-                                        .collect(Collectors.joining(", "));
-                            }
-                            exercises.add(result);
-                        }
-                    }
-                    ArrayAdapter adaptExercises = new ArrayAdapter<String>(
-                            context,
-                            android.R.layout.simple_list_item_1,
-                            exercises);
-                    ChooseExercise.setAdapter(adaptExercises);
-                }
-            });
+        getDefaultExercises();
 
         GoBackToWorkoutButton = (Button) findViewById(R.id.goBackToWorkoutButton);
         GoBackToWorkoutButton.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/fitwork/CreateWorkoutActivity.java
+++ app/src/main/java/com/example/fitwork/CreateWorkoutActivity.java
@@ -12,11 +12,20 @@ import androidx.annotation.Nullable;
 public class CreateWorkoutActivity extends Activity {
 
     private Button addExerciseButton;
+    private Button backTestButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workout_form);
+
+        backTestButton = (Button) findViewById(R.id.backTestButton);
+        backTestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreateWorkoutActivity.this, DashboardActivity.class));
+            }
+        });
 
         addExerciseButton = (Button) findViewById(R.id.addWorkoutExerciseButton);
         addExerciseButton.setOnClickListener(new View.OnClickListener() {
